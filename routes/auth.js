d35163bca40a0f526668ac9ccda290a62de96159
PrinceRohara <75170476+PrinceRohara@users.noreply.github.com>
2023-03-07 18:04:01 +0530

    Update auth.js
    password

--- routes/auth.js
+++ routes/auth.js
@@ -45,7 +45,7 @@ router.post("/login", async (req, res) => {
     }
 
     const validated = await bcrypt.compare(req.body.password, user.password);
-    // !validated && res.status(400).json("wrong credentials!");
+    !validated && res.status(400).json("wrong credentials!");
 
     // if (!validated) {
     //   res.status(400).json("wrong Credentials");
